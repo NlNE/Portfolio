de9dcb4c8957844a1fc7b655dc1166b3f25f95ea
NINE <60499994+NlNE@users.noreply.github.com>
2020-03-09 08:16:00 +0900

    모바일 테스트

--- docs/js/slide-menu.js
+++ docs/js/slide-menu.js
@@ -11,8 +11,6 @@ function showSlideMenu() {
     slideMenu.style['webkitTransform'] = "translate(0px, 0px)";
     slideMenu.style['oTransform'] = "translate(0px, 0px)";
     slideWrap.classList.add('menu-active');
-
-    document.querySelector('body').style['overflow'] = 'hidden';
 }
 
 function closeSlideMenu() {
@@ -23,5 +21,5 @@ function closeSlideMenu() {
     btnSlideOpen.style['display'] = "block";
     slideMenu.removeAttribute("style");
     slideWrap.classList.remove('menu-active');
-    setTimeout(function(){        document.querySelector('body').removeAttribute("style")},1000);
+//    setTimeout(function(){        document.querySelector('body').removeAttribute("style")},1000);
 }
