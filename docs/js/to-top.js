51286f0754e5e21cf1fe5b26acc9e95e9d839f61
NINE <60499994+NlNE@users.noreply.github.com>
2020-03-19 02:11:33 +0900

    사파리 smooth scroll 이슈 해결

--- docs/js/to-top.js
+++ docs/js/to-top.js
@@ -1,19 +1,11 @@
-document.getElementById("to-top").addEventListener('click', function () {
-    //alert("위로 버튼을 클릭했습니다.");
-    var body = document.getElementsByTagName("body")[0];
-    window.scroll({
-        behavior: 'smooth',
-        left: 0,
-        top: body.offsetTop
+( function( $ ) {
+    $( document ).ready(function() {
+ 
+         $('#to-top').click(
+         function () { $('body,html').animate({ scrollTop: 0 }, 460);}
+         )
+ 
     });
-});
-
-//document.getElementById("downBtn").addEventListener('click', function () {
-//    //alert("아래로 버튼을 클릭했습니다.");
-//    var body = document.getElementsByTagName("body")[0];
-//    window.scroll({
-//        behavior: 'smooth',
-//        left: 0,
-//        top: body.offsetHeight
-//    });
-//});
+ 
+}
+)( jQuery );
