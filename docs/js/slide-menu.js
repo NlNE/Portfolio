691509fe9f0aeb35f2cef62e702878384a5946eb
NINE <60499994+NlNE@users.noreply.github.com>
2020-03-21 20:04:42 +0900

    gnb 관련 수정
    - 스크롤 시 색상변경
    - 서브페이지 gnb css파일 분리

--- docs/js/slide-menu.js
+++ docs/js/slide-menu.js
@@ -26,24 +26,24 @@ function closeSlideMenu() {
     slideWrap.classList.remove('menu-active');
     if (window.innerWidth < 768) {
         setTimeout(function () {
-        document.querySelector('body').removeAttribute("style")
+            document.querySelector('body').removeAttribute("style")
         }, 600);
     };
 }
 
 $(function () {
     var slideMenu = $('#btn-slide-open'); //색상이 변할 부분
-    var pageStart = $('.logo-wrap')
-    var pageOffsetTop = pageStart.offset().top; //색상 변할 부분의 top값 구하기
-    $(window).resize(function () { //반응형을 대비하여 리사이즈시 top값을 다시 계산
-        pageOffsetTop = pageStart.offset().top;
-    });
+    var slideMenuIcon = $('.css-slide-open .line'); //색상 변경될 아이콘 라인
 
     $(window).on('scroll', function () {
-        if ($(window).scrollTop() >= pageOffsetTop) { // 스크롤이 page보다 밑에 내려가면
+        if ($(window).scrollTop() >= 60) { // 스크롤이 page보다 밑에 내려가면
             slideMenu.addClass('btn-slide-open-scrollDown'); //클래스 추가
+            slideMenuIcon.addClass('line-scrollDown');
+
         } else { // 스크롤 올릴 때
             slideMenu.removeClass('btn-slide-open-scrollDown'); //클래스 제거
+
+            slideMenuIcon.removeClass('line-scrollDown');
         }
     });
 });
