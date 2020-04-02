a8cc45cf93d0b6c69c73e5ffce43de4c94b4debe
NINE <60499994+NlNE@users.noreply.github.com>
2020-04-02 23:48:13 +0900

    로딩 씨발

--- docs/js/boxSizing.js
+++ docs/js/boxSizing.js
@@ -1,11 +1,12 @@
-$(document).ready(function () {    
+$(document).ready(function () {
     var scrollBoxWidth = 0;
     var contentWidth = $('.content');
 
     contentWidth.each(function () {
         scrollBoxWidth = scrollBoxWidth + $(this).width();
     })
-
+    
+    $('.loading').fadeOut();
     $('.content-scroll-box').css('width', scrollBoxWidth);
 
     if (window.innerWidth <= 768) {
