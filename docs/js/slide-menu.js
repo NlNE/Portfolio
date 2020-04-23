a4c138350eda86b953c168e4644e75a536d96919
NINE <60499994+NlNE@users.noreply.github.com>
2020-04-23 20:11:23 +0900

    자자
    자자자자 가봅시다!

--- docs/js/slide-menu.js
+++ docs/js/slide-menu.js
@@ -1,4 +1,4 @@
-$(window).on('load', function(){
+$(window).on('load', function () {
     $('.loading').fadeOut();
 });
 
@@ -36,6 +36,17 @@ function closeSlideMenu() {
     };
 }
 
+function showStory() {
+    var btnStoryOpen = document.querySelector('.story');
+    var slidePage = document.querySelector('.slide-page-wrap');
+
+    slidePage.style['transform'] = "translate(0px, 0px)";
+    slidePage.style['msTransform'] = "translate(0px, 0px)";
+    slidePage.style['mozTransform'] = "translate(0px, 0px)";
+    slidePage.style['webkitTransform'] = "translate(0px, 0px)";
+    slidePage.style['oTransform'] = "translate(0px, 0px)";   
+}
+
 $(function () {
     var slideMenu = $('#btn-slide-open'); //색상이 변할 부분
     var slideMenuIcon = $('.css-slide-open .line'); //색상 변경될 아이콘 라인
@@ -47,6 +58,23 @@ $(function () {
 
         } else { // 스크롤 올릴 때
             slideMenu.removeClass('btn-slide-open-scrollDown'); //클래스 제거
+
+            slideMenuIcon.removeClass('line-scrollDown');
+        }
+    });
+});
+
+$(function () {
+    var slideMenu = $('#btn-slide-back'); //색상이 변할 부분
+    var slideMenuIcon = $('.css-slide-back .line'); //색상 변경될 아이콘 라인
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() >= 60) { // 스크롤이 page보다 밑에 내려가면
+            slideMenu.addClass('btn-slide-back-scrollDown'); //클래스 추가
+            slideMenuIcon.addClass('line-scrollDown');
+
+        } else { // 스크롤 올릴 때
+            slideMenu.removeClass('btn-slide-back-scrollDown'); //클래스 제거
 
             slideMenuIcon.removeClass('line-scrollDown');
         }
